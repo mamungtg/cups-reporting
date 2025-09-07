c06287c38b26e58f036390510f20a77125c0d345
Mominul Hoque <hoque.mdmominul@gmail.com>
2025-09-07 18:10:32 +0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 set -e
 
-# Ensure jobs.db exists
+# Ensure jobs.db exists at runtime
 if [ ! -f /app/jobs.db ]; then
   echo "Initializing jobs.db..."
   sqlite3 /app/jobs.db < /app/init.sql
+  chown -R $(whoami) /app/jobs.db
 fi
 
-# Start CUPS in foreground (backgrounded here)
+# Start CUPS
 echo "Starting CUPS..."
 cupsd -f &
 
 # Start Flask app
-echo "Starting Flask reporting app..."
+echo "Starting Flask..."
 python3 /app/app.py
